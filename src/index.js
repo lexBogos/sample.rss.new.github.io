1271399e8dbfb753dd0775e2d1907e0319d31f7f
lexBogos <lex.bogos@gmail.com>
2018-11-29 22:18:40 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,162 +1,95 @@
 import test from './test'
 
-test('!!!')
-
+test("les't start")
 
 let newdiv = document.createElement("div");
 document.body.appendChild(newdiv);
-newdiv.style.marginRight="auto";
-newdiv.style.marginLeft="auto";
-newdiv.style.minHeight= "945px";
-newdiv.style.backgroundColor = '#343D46';
+newdiv.className = 'newdiv';
+
 
 let newInp = document.createElement("input");
-newInp.style.height="25px";
-newInp.style.width="74%";
-newInp.style.borderRadius='10px';
+newInp.className = 'inp';
+newInp.placeholder="press 'Enter' after entering" ;
 newdiv.appendChild(newInp);
 
 let newdiv1 = document.createElement("div");
-newdiv1.style.marginTop="50px";
-
-newdiv1.style.justifyContent = "space-between";
-newdiv1.style.width="90%";
-newdiv1.style.marginRight="auto";
-newdiv1.style.marginLeft="auto";
-
 newdiv1.className = 'quzas';
 newdiv.appendChild(newdiv1);
 
 let iva = document.createElement("div");
 iva.className = 'iva';
-iva.style.minHeight="300px";
-iva.style.textAlign="center";
-iva.style.margin="30px auto";
-iva.style.paddingTop = '50px';
-iva.style.backgroundColor = 'white';
-iva.style.borderRadius ='30px'; 
 newdiv1.appendChild(iva);
 
 let iva1 = document.createElement("div");
 iva1.className = 'iva1';
-iva1.style.textAlign="center";
-iva1.style.margin="30px auto";
-iva1.style.paddingTop = '50px';
-iva1.style.backgroundColor = 'white';
-iva1.style.borderRadius ='30px';
-iva1.style.minHeight="300px";
 newdiv1.appendChild(iva1);
 
 let iva2 = document.createElement("div");
 iva2.className = 'iva2';
-iva2.style.textAlign="center";
-iva2.style.margin="30px auto";
-iva2.style.paddingTop = '50px';
-iva2.style.backgroundColor = 'white';
-iva2.style.borderRadius ='30px';
-iva2.style.minHeight="300px";
 newdiv1.appendChild(iva2);
 
 let iva3 = document.createElement("div");
 iva3.className = 'iva3';
-iva3.style.textAlign="center";
-iva3.style.margin="30px auto";
-iva3.style.paddingTop = '50px';
-iva3.style.backgroundColor = 'white';
-iva3.style.borderRadius ='30px';
-iva3.style.minHeight="300px";
 newdiv1.appendChild(iva3);
 
 let container = document.createElement("div");
 container.className='cont';
-container.style.minWidth="100%";
-container.style.margin="0 auto";
+
 
 let container1 = document.createElement("div");
 container1.className='cont1';
-container1.style.width="90%";
-container1.style.textAlign="center";
-container1.style.margin="0 auto";
+
 
 let container2 = document.createElement("div");
-container2.style.position = "relative";
 container2.className='cont2';
-container2.style.backgroundColor = "rgb( 255, 255, 255)";
-container2.style.backgroundColor = "rgba( 15, 98, 64, 0.8)";
-container2.style.top="-100px";
+
 
 let container3 = document.createElement("div");
 container3.className='cont';
-container3.style.width="90%";
-container3.style.margin="0 auto";
+
 
 let container4 = document.createElement("div");
 container4.className='cont1';
-container4.style.width="100%";
-container4.style.textAlign="center";
-container4.style.margin="0 auto";
+
 
 let container5 = document.createElement("div");
-container5.style.position = "relative";
 container5.className='cont2';
-container5.style.backgroundColor = "rgba( 15, 98, 64, 0.8)";
-container5.style.top="-100px";
+
 
 let container6 = document.createElement("div");
 container6.className='cont';
-container6.style.width="90%";
-container6.style.margin="0 auto";
+
 
 let container7 = document.createElement("div");
 container7.className='cont1';
-container7.style.width="100%";
-container7.style.textAlign="center";
-container7.style.margin="0 auto";
+
 
 let container8 = document.createElement("div");
-container8.style.position = "relative";
 container8.className='cont2';
-container8.style.backgroundColor = "rgba( 15, 98, 64, 0.8)";
-container8.style.top="-100px";
+
 
 let container9 = document.createElement("div");
 container9.className='cont';
-container9.style.width="90%";
-container9.style.margin="0 auto";
+
 
 let container10 = document.createElement("div");
 container10.className='cont1';
-container10.style.width="100%";
-container10.style.textAlign="center";
-container10.style.margin="0 auto";
 
 let container11 = document.createElement("div");
-container11.style.position = "relative";
 container11.className='cont2';
-container11.style.backgroundColor = "rgba( 15, 98, 64, 0.8)";
-container11.style.top="-100px";
+
 
 let arrow_minus= document.createElement("a");
 arrow_minus.href = "#";
-arrow_minus.style.textDecoration = 'none';
-arrow_minus.style.fontSize = "30px";
-arrow_minus.style.color = "rgb(255,205,67)";
-arrow_minus.style.marginLeft = "45%";
 arrow_minus.className = 'arrow_minus'; 
 newdiv.appendChild(arrow_minus);
 
 let arrow_plus = document.createElement("a"); 
 arrow_plus.href = "#";
-arrow_plus.style.textDecoration = 'none';
-arrow_plus.style.fontSize = "30px";
-arrow_plus.style.color = "rgb(255,205,67)";
-arrow_plus.style.marginLeft = "8.5%";
 arrow_plus.className = 'arrow_plus';
 newdiv.appendChild(arrow_plus);
 
 let page_num = document.createElement("div"); 
-page_num.style.color = "rgb(255,205,67)";
-page_num.style.marginLeft = "8.5%";
 page_num.className = 'page_nums';
 newdiv.appendChild(page_num);
 
@@ -177,11 +110,23 @@ function searcher(){
 }
   
     function updateDisplay(searcherValue) {
-        if (curr_pos>help_arr_for_videos.length){
+        if (curr_pos>24){
+            curr_pos=0
             pageToken='pageToken='+arr_for_slyder[arr_for_slyder.length-1]+'&'
             if(pageToken === 'pageToken=undefined&'){
                 pageToken = '';
-              }
+              }  
+            }
+         if (curr_pos<0){
+            curr_pos=0
+            if(arr_for_slyder.length>0){
+              curr_pos=20
+            arr_for_slyder.pop()
+            pageToken='pageToken='+arr_for_slyder[arr_for_slyder.length-1]+'&'
+            if(pageToken === 'pageToken=undefined&'){
+                pageToken = '';
+                }
+              }  
             }
         let url = 'https://www.googleapis.com/youtube/v3/search?'+pageToken+'part=snippet&maxResults=24&type=video&q=' + searcherValue + '&key=AIzaSyA-49FCq9Ez_hSOdQ2pL5aR1pkog6kMmnU';
         console.log('url',url)
@@ -191,12 +136,12 @@ function searcher(){
       	request.responseType = "json";
       	
       	request.onload = function() {
-      		// console.log(request.response)
-          if(curr_pos===help_arr_for_videos.length){ 
+          console.log('curr_pos', curr_pos)
+          if(curr_pos===help_arr_for_videos.length&&help_arr_for_videos.length>0){ 
             console.log('curr_pos===help_arr_for_videos.length')
              arr_for_slyder.push(String(request.response.nextPageToken))
-             console.log('arr_for_slyder', arr_for_slyder) 
-          } 
+             console.log('arr_for_slyder', arr_for_slyder)
+            } 
       		help_arr_for_videos = [];
       		for (let k=0; k<=24; k++){
      	 			help_arr_for_videos.push(String(request.response.items[k].id.videoId))
@@ -205,9 +150,7 @@ function searcher(){
 
      	request.send();
 
-     	// console.log("help_arr_for_videos", help_arr_for_videos)
-      
-      let url_1 ='';
+     let url_1 ='';
 
       for(let i =0; i<=curr_pos; i+=4){
      	url_1 = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyA-49FCq9Ez_hSOdQ2pL5aR1pkog6kMmnU&id='+help_arr_for_videos[i]+','+help_arr_for_videos[i+1]+','+help_arr_for_videos[i+2]+','+help_arr_for_videos[i+3]+','+'&part=snippet,statistics'
@@ -240,23 +183,15 @@ function searcher(){
 
       	     };
         };
-       page_num.innerHTML = 'page number:' + page_number  ; 
+       page_num.innerHTML = 'page number: ' + page_number  ; 
      	request_1.send();
      };
 
         arrow_plus.addEventListener('click',  next_slide);
         function next_slide (){  
         page_number  += 1 ;
-        if (curr_pos<=help_arr_for_videos.length){
-            curr_pos+=4
-            updateDisplay(newInp.value)
-          }
-        else{
-          curr_pos=0
-          updateDisplay(newInp.value)
-          console.log('arr_for_slyder')
-          console.log(arr_for_slyder)
-          }  
+        curr_pos+=4
+        updateDisplay(newInp.value)
         };
 
         arrow_minus.addEventListener('click', prev_slide);
@@ -264,26 +199,11 @@ function searcher(){
           if (page_number>1){ 
            page_number -= 1 ;
           }
-           if (curr_pos-4 >= 0){
-              curr_pos-=4
-            }
-           else{
-            arr_for_slyder.pop()
-            curr_pos = 20
-          console.log('arr_for_slyder')
-          console.log(arr_for_slyder)
-          }
-           updateDisplay(newInp.value)
-           
-        }; 
+            curr_pos-=4
+            updateDisplay(newInp.value)
+        }
+    
 
 updateDisplay('')
 
-     // <div class="nav_block_click_item">
-     //    <span class="block_click_item" onclick="slide(0)" style="background: transparent;"></span>
-     //    <span class="block_click_item" onclick="slide(1)" style="background: transparent;"></span>
-     //    <span class="block_click_item" onclick="slide(2)" style="background: rgb(0, 0, 0);"></span>
-     //    <span class="block_click_item" onclick="slide(3)" style="background: transparent;"></span>
-     //    <span class="block_click_item" onclick="slide(4)" style="background: transparent;"></span>
-     //    <span class="block_click_item" onclick="slide(5)" style="background: transparent;"></span>
-     //  </div>  
+
--- src/test.js
+++ src/test.js
@@ -1,6 +1,3 @@
 export default function test(name = 'noname'){
 	console.log(name)
 }
-
-
-// GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key={YOUR_API_KEY}
